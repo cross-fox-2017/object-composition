89fda79d0e324ef4914f3f7aa908cf684a31fc49
Syanmil <syanmil@yahoo.com>
2017-01-10 20:43:43 +0700

    done with class in class in class :D

--- composition.js
+++ composition.js
@@ -2,75 +2,111 @@
 const fs = require('fs')
 
 class Cookie{
-  constructor(inggridient){
-    this.status = 'Matang'
-    this.inggridient = inggridient
-    this.sugar = 200
-    this.cinnamon = 50
+  constructor(bahan){
+    this.bahan = this.olah(bahan)
+  }
+  olah(bahan){
+    let bahanbaku = []
+    bahan = bahan.toString().split(",");
+    for (let i = 0; i < bahan.length; i++){
+      bahan[i] = bahan[i].split(" : ")
+      bahanbaku.push(new Inggridient(bahan[i][1], bahan[i][0]))
+    }
+    return bahanbaku
   }
 }
 
 class PeanutButter extends Cookie {
-  constructor(status, sugar, cinnamon) {
-    super(status, sugar, cinnamon)
-    this.peanut_count = 100
-    this.sugar = 250
-    this.cinnamon = 20
+  constructor(bahan) {
+    super(bahan)
+    this.peanut_count = 150
   }
 }
-
 class ChocolateChip extends Cookie{
-  constructor(status, sugar, cinnamon) {
-    super(status, sugar, cinnamon)
-    this.choc_chip_count = 200
-    this.sugar = 100
-    this.cinnamon = 0
+  constructor(bahan) {
+    super(bahan)
+    this.choco_count = 200
+  }
+}
+class ChocolateCheese extends Cookie{
+  constructor(bahan) {
+    super(bahan)
+    this.cheese_count = 80
+  }
+}
+class ChocolateButter extends Cookie{
+  constructor(bahan) {
+    super(bahan)
+    this.butter = 120
   }
 }
 
 class Inggridient{
-  constructor(options){
-    this.name = options['name']
-    this.amount = options['amount']
-    this.has_gluten = options['has_gluten']
+  constructor(name, amount){
+    this.name = name
+    this.amount = amount
   }
 }
 
 class CookieFactory{
-  constructor(){
-    this.options = fs.readFileSync('cookies.txt', 'utf8').split("\n");
+  constructor(file){
+    this.file = fs.readFileSync(file, 'utf8').trim().split("\n")
+    this.options = []
     this.product = [];
-    this.sugarCek = 0;
-    this.cinnanmonCek = 0;
+    this.sugarCek = [];
+  }
+  processFile(){
+    for (let i = 0; i < this.file.length; i++){
+      this.file[i] = this.file[i].trim().split(" = ")
+      this.file[i][1] = this.file[i][1].trim().split(", ")
+    }
+    return this.options = this.file
   }
   createFromList(){
     let production = []
-    this.options.forEach(function(val){
-      switch (val) {
-        case 'PeanutButter':
-          production.push(new PeanutButter())
+    for (let i = 0; i < this.file.length; i++){
+      this.file[i] = this.file[i].trim().split(" = ")
+      this.file[i][1] = this.file[i][1].trim().split(", ")
+    }
+    for (let i = 0; i < this.file.length; i++){
+      let cari = this.file[i][0]
+      let bahan = this.file[i][1]
+      switch (cari){
+        case 'peanut butter':
+          production.push(new PeanutButter(bahan))
           break;
-        case 'ChocolateChip':
-          production.push(new ChocolateChip())
+        case 'chocolate chip':
+          production.push(new ChocolateChip(bahan))
+          break;
+        case 'chocolate cheese':
+          production.push(new ChocolateCheese(bahan))
+          break;
+        case 'chocolate butter':
+          production.push(new ChocolateButter(bahan))
+          break;
         default:
-          production.push(new Cookie())
+          production.push(new Cookie(bahan))
       };
-    });
+    }
     return this.product = production
   }
   inggridientCheck(){
-    let that = this
-    this.product.forEach(function(val){
-      that.sugarCek += val.sugar
-      that.cinnanmonCek += val.cinnamon
-    })
-    return `This list have ${this.sugarCek} gr of sugar, and ${this.cinnanmonCek} gr of cinnamon`
+    for (let i = 0; i < this.product.length; i++){
+      for (let j = 0; j < this.product[i].bahan.length; j++){
+        if (this.product[i].bahan[j].name == 'sugar'){
+          this.sugarCek.push(this.product[i])
+          break;
+        }
+      }
+    }
+    return `This Product ${this.sugarCek} has sugar on it, Not Recomended`
   }
 
 }
 
-let fact = new CookieFactory()
+let fact = new CookieFactory('recipes.txt')
 let batchOfCookies = fact.createFromList()
+// fact.processFile()
 // console.log(fact.options);
 console.log(batchOfCookies);
-console.log(fact.inggridientCheck( ));
+console.log(fact.inggridientCheck());
