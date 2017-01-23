2f5f46b49bcd5bdfb7bfac34aaf86832d11bfb52
Raditya <arya.raditya26@gmail.com>
2017-01-23 22:09:52 +0700

    done

--- composition.js
+++ composition.js
@@ -1,92 +1,117 @@
 "use strict"
-
 const fs = require('fs')
 
-class Cookie {
-  constructor(){
-    this.status = 'mentah'
+class Cookie{
+  constructor(name,ingredients){
+    this.name= name
+    this.ingredients = ingredients
   }
+}
 
-  bake(ing){
-    let ingredients = []
-    ing = ing.toString().split(',')
-    for (var i = 0; i < ing.length; i++) {
-      ing[i] = ing[i].split(" : ")
-      ingredients.push(new Composition(ing[i][1], ing[i][0]))
-    }
-    return ingredients
+class Ingredients {
+  constructor(name,amount){
+    this.name = name
+    this.amount = amount
   }
 }
 
 class PeanutButter extends Cookie {
-  constructor(bahan) {
-    super(bahan)
-    this.peanut_count = 100
+  constructor(name,ingredients){
+    super(name,ingredients)
   }
 }
 
-class ChocolateChip extends Cookie {
-  constructor(bahan){
-    super(bahan)
-    this.choc_chip_count = 200
+class ChocolateChip extends Cookie{
+  constructor(name,ingredients){
+    super(name,ingredients)
   }
 }
-
-// class RainbowCake extends Cookie {
-//   constructor() {
-//     this.rainbow_count = 300
-//   }
-// }
 
 class ChocolateCheese extends Cookie{
-  constructor(bahan) {
-    super(bahan)
-    this.cheese_count = 80
+  constructor(name,ingredients){
+    super(name,ingredients)
   }
 }
+
 class ChocolateButter extends Cookie{
-  constructor(bahan) {
-    super(bahan)
-    this.butter = 120
+  constructor(name,ingredients){
+    super(name,ingredients)
   }
 }
 
-class Composition {
-  constructor(name, total) {
-    this.name = name
-    this.total = total
+function cook(array){
+  var num=[]
+  for(var j=0; j<array.length;j++){
+    num.push(array[j].split(' : '))
   }
+  var getA=[]
+  var getB=[]
+  var getC=[]
+  for(var o=0;o<num.length;o++){
+    getA.push(num[o][0])
+    getB.push(num[o][1])
+  }
+  var obj =[]
+  for(var l=0;l<getA.length;l++){
+    obj.push(new Ingredients(getB[l],getA[l]))
+  }
+  return obj
 }
 
-class CookiesFactory {
-  constructor(file) {
-      this.list = fs.readFileSync(file, 'UTF-8').trim().split('\n')
-      this.cookies = []
+class CookieFactory{
+  constructor(){
+    this.results=[]
   }
+  create(options){
+    var recipes = fs.readFileSync(options,'utf-8').trim().split('\n');
+    var recipesSplit=[]
+    var cookies=[]
+    var ingredients=[]
+    var composition=[]
 
-  cook(){
-    let make = []
-    for (var i = 0; i < this.list.length; i++) {
-      this.list[i] = this.list[i].trim().split(' = ')
-      this.list[i][1] = this.list[i][1].trim().split(", ")
-
-      let a = this.list[i][0]
-      let bahan = this.list[i][1]
-      if (a == 'peanut butter')
-        make.push(new PeanutButter(bahan))
-      else if (a == 'chocolate chip')
-        make.push(new ChocolateChip(bahan))
-      else if (a == 'chocolate cheese')
-        make.push(new ChocolateCheese(bahan))
-      else if (a == 'chocolate butter')
-        make.push(new ChocolateButter(bahan))
-      else
-        make.push(new Cookie(bahan))
+    for(var i=0; i<recipes.length;i++){
+      recipesSplit.push(recipes[i].split(' = '))
     }
-    return this.cookies = make
+    for(var i=0;i<recipesSplit.length;i++){
+      cookies.push(recipesSplit[i][0])
+      ingredients.push(recipesSplit[i][1])
+    }
+    for(var i=0;i<ingredients.length;i++){
+      composition.push(ingredients[i].split(','))
+    }
+
+    for(var i=0; i< cookies.length;i++){
+      if(cookies[i] === "peanut butter"){
+        var j= cook(composition[i])
+        this.results.push(new PeanutButter(cookies[i],j))
+      }
+      else if(cookies[i] === "chocolate chip"){
+        var j= cook(composition[i])
+        this.results.push(new ChocolateChip(cookies[i],j))
+      }
+      else if(cookies[i] === "chocolate cheese"){
+        var j= cook(composition[i])
+        this.results.push(new ChocolateCheese(cookies[i],j))
+      }
+      else if(cookies[i] === "chocolate butter"){
+        var j= cook(composition[i])
+        this.results.push(new ChocolateButter(cookies[i],j))
+      }
+    }
+     return this.results
   }
+
+  // cookieRecommendation(){
+  //   console.log( this.results.ingredients);
+  // // for(let i = 0; i < this.results.length; i++){
+  // //   for(let j = 0; j < this.results[i].ingredients.length; j++){
+  // //     if(this.results[i].ingredients[j]['ingredients'] === 'gluten free flour'){
+  // //       return this.results[i].name
+  // //     }
+  // //   }
+  // // }
+// }
 }
 
-  let test = new CookiesFactory('cookies.txt')
-  let batchOfCookies = test.cook()
-  console.log(batchOfCookies);
+let cookieFactory= new CookieFactory
+console.log(JSON.stringify(cookieFactory.create('cookies.txt')))
